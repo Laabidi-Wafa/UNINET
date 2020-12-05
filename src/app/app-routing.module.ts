de331b9f6e58b3193571ae6f22c36c128c6e4f99
Laabidi-Wafa <laabidiwafa199@gmail.com>
2020-12-05 21:33:46 +0100

    Controle saisie login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/sign-up', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'sign-up', component: SignupComponent },
   { path: 'login', component: LoginComponent },
